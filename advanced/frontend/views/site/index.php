5bceadcd0a03f7c8865efff6f2f0224baf487f68
Susi Eva <susipurba2@gmail.com>
2018-08-03 10:58:51 +0700

    no message

---
+++ advanced/frontend/views/site/index.php
@@ -0,0 +1,44 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Congratulations!</h1>
+
+        <p class="lead">You have successfully created your Yii-powered application.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">DAFTAR</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Intensive Class</h2>
+
+                <p>Kelas Intensif merupakan program belajar pemrograman Web, Android, dan desain yang dilakukan secara intensif selama satu minggu dengan sistem karantina.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Baca Selengkapnya &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Regular Class</h2>
+
+                <p>Regular Class diperuntukan bagi Anda yang ingin belajar programming dan desain diwaktu senggang (pagi, siang atau malam) dengan 12 X jumlah pertemuan.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Baca Selengkapnya &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Privat Class</h2>
+
+                <p>Kelas Privat diperuntukan untuk Anda yang ingin memperlajari pemrograman dan desain secara mandiri dengan pemilihan waktu yang lebih fleksibel.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Baca Selengkapnya &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
